a2287748392722af488ee62b5f4e7168f0b2ef83
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-05-06 11:03:33 +0530

    ...

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 import musicModel from "../models/musics.js";
 import handleDirectUploadUpload from "../handlers/handleDirectUpload.js";
-import sanitizeYouTubeURL from "../utils/sanitizeUrl.js";
+
 
 const router = express.Router();
 const storage = multer.memoryStorage(); // Use memory storage for quick uploads
@@ -20,6 +20,24 @@ const downloadFileAsBuffer = async url => {
         return Buffer.from(response.data);
     } catch (err) {
         console.error("Error downloading file:", err.message);
+        return null;
+    }
+};
+
+const sanitizeYouTubeURL = url => {
+    try {
+        url = url
+            .trim()
+            .replace("m.youtube.com", "www.youtube.com")
+            .replace("youtu.be/", "www.youtube.com/watch?v=")
+            .split("&")[0]; // remove extra query params
+
+        const parsed = new URL(url);
+        const videoId = parsed.searchParams.get("v");
+        if (!videoId) return null;
+
+        return `https://www.youtube.com/watch?v=${videoId}`;
+    } catch {
         return null;
     }
 };
