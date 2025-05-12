422bbc33c63315c4ae352230bcc0164c7217762f
AdwaithAnandSR <adwaithanand1818@gmail.com>
2025-05-12 21:36:52 +0530

    moved to playdl

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,8 +6,8 @@ import ytdl from "ytdl-core";
 import play from "play-dl";
 
 // import musicModel from "../models/musics.js";
-import streamToBuffer from "../utils/streamToBuffer.js";
-import handleDirectUploadUpload from "../handlers/handleDirectUpload.js";
+// import streamToBuffer from "../utils/streamToBuffer.js";
+// import handleDirectUploadUpload from "../handlers/handleDirectUpload.js";
 
 const router = express.Router();
 const storage = multer.memoryStorage(); // Use memory storage for quick uploads
@@ -36,25 +36,49 @@ router.post("/saveToCloud", async (req, res) => {
             let { cookie, url } = req.body;
             url = sanitizeYouTubeURL(url);
             console.log("\nurl: ", url);
-            
+
             await play.setToken({
                 youtube: {
                     cookie: cookie,
-                    client: "WEB" 
+                    client: "WEB"
                 }
             });
-            
+
             const isvalid = await play.yt_validate(url);
-            console.log("\visvalid : ", isvalid);
-            
+            if (!isvalid)
+                return res.status(403).json({
+                    message: "not a valid url"
+                });
+
             const info = await play.video_info(url);
-            console.log("\ninfo : ", info);
+            console.log("\ntitle : ", info.video_details.title);
+
+            const stream = await play.stream(videoURL);
+
+            const audioChunks = [];
+            stream.stream.on("data", chunk => audioChunks.push(chunk));
+            await new Promise(resolve => stream.stream.on("end", resolve));
+
+            const audioBuffer = Buffer.concat(audioChunks);
+
+            // Step 3: Download and buffer the cover image
+            const thumbnailURL = info.video_details.thumbnails.pop().url;
+            const coverRes = await axios.get(thumbnailURL, {
+                responseType: "arraybuffer"
+            });
+            const coverBuffer = Buffer.from(coverRes.data);
+
+            // Now you can use audioBuffer and coverBuffer
+            console.log("audioBuffer size:", audioBuffer.length);
+            console.log("coverBuffer size:", coverBuffer.length);
+
+            const audioType = await fileTypeFromBuffer(audioBuffer);
+            const coverType = await fileTypeFromBuffer(coverBuffer);
+
+            console.log(audioType, coverType);
         } catch (error) {
             console.log(error);
         }
-
-        // const audioType = await fileTypeFromBuffer(audioBuffer);
-        // const coverType = await fileTypeFromBuffer(coverBuffer);
 
         // handleDirectUploadUpload({
         //     title,
