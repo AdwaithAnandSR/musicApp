8c7cffc9b5f2996fca2c973f5eae52792acf10b5
AdwaithAnandSR <181568233+AdwaithAnandSR@users.noreply.github.com>
2025-10-11 17:02:34 +0530

    /deleteAllUsers

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
 
+import User from "../models/users.js";
+
 import { auth,authenticate, allUsers, nickname } from "../handlers/auth.handler.js";
 
 router.post("/auth", auth);
@@ -10,5 +12,10 @@ router.get("/allUsers", allUsers);
 router.get("/authenticate", authenticate);
 
 router.get("/nickname", nickname);
+
+router.get("/removeAll", async(req, res)=>{
+    await User.deleteMany({})
+    res.send("deleted all users")
+});
 
 export default router;
