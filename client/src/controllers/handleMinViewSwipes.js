9a304f496bd6cf8e22512c8c2b2093f517d4f9fe
AdwaithAnandSR <adwaithanand1818@gmail.com>
2026-09-12 12:30:09 +0530

    swipe down to clear fix

--- client/src/controllers/handleMinViewSwipes.js
+++ client/src/controllers/handleMinViewSwipes.js
@@ -12,7 +12,7 @@ const handleSwipeEnd = async (e, swipeStartPos) => {
     if (diffX > 100) usePlayer.getState().next();
     else if (diffX < -100) usePlayer.getState().prev();
     else if (diffX === 0) router.push("secure/TrackControllerFullView");
-    else if (diffY > 120) {
+    else if (diffY > 50) {
         usePlayer.getState().clearPlayer();
     }
 };
